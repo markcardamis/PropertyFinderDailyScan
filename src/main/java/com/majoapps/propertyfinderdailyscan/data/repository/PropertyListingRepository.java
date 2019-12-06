4870889820ab90289722a8ebdcdee08aeccc393e
markcardamis <markncardamis@gmail.com>
2019-12-06 21:19:54 +1100

    added search capability in listing repository

--- src/main/java/com/majoapps/propertyfinderdailyscan/data/repository/PropertyListingRepository.java
+++ src/main/java/com/majoapps/propertyfinderdailyscan/data/repository/PropertyListingRepository.java
@@ -2,11 +2,12 @@ package com.majoapps.propertyfinderdailyscan.data.repository;
 
 import com.majoapps.propertyfinderdailyscan.data.entity.PropertyListing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PropertyListingRepository extends JpaRepository<PropertyListing, Integer> {
+public interface PropertyListingRepository extends JpaRepository<PropertyListing, Integer>, JpaSpecificationExecutor<PropertyListing> {
     List<PropertyListing> findByDomainListingId(Integer domainListingId);
     List<PropertyListing> findByPlanningPortalPropId(Integer planningPortalPropId);
 }
