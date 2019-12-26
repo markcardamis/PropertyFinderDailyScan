d7d8d8b10bf65c5a2600bea188b73a13d73fcaec
markcardamis <markncardamis@gmail.com>
2019-12-26 23:51:08 +1100

    added log on exception handler on daily scan

--- src/main/java/com/majoapps/propertyfinderdailyscan/MyRunner.java
+++ src/main/java/com/majoapps/propertyfinderdailyscan/MyRunner.java
@@ -1,6 +1,7 @@
 package com.majoapps.propertyfinderdailyscan;
 
 import com.majoapps.propertyfinderdailyscan.scheduled.DailyPropertyScan;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Component
 public class MyRunner implements CommandLineRunner {
     
@@ -20,7 +22,11 @@ public class MyRunner implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        dailyPropertyScan.getListingsNSW();
+    public void run(String... args) {
+        try {
+            dailyPropertyScan.getListingsNSW();
+        } catch (Exception e) {
+            log.error("get Listing exception {} ", e);
+        }
     }
 }
