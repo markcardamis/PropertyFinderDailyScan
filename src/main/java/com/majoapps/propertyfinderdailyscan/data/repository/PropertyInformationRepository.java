9162eec330669409d99a492cddbb5e8751076ec5
markcardamis <markncardamis@gmail.com>
2019-12-29 00:56:36 +1100

    testing add tsv column

--- src/main/java/com/majoapps/propertyfinderdailyscan/data/repository/PropertyInformationRepository.java
+++ src/main/java/com/majoapps/propertyfinderdailyscan/data/repository/PropertyInformationRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PropertyInformationRepository extends JpaRepository<PropertyInformation, Integer> {
 
-        @Query(value = "SELECT concat_ws(',', property_id, concat_ws(' ', unit_number, house_number, street_name, suburb_name, post_code)) FROM property_information WHERE  to_tsvector('simple', f_concat_ws(' ', unit_number, house_number, street_name, suburb_name, post_code)) @@ plainto_tsquery('simple', ?1) LIMIT 1", nativeQuery = true)
+        @Query(value = "SELECT concat_ws(',', property_id, concat_ws(' ', unit_number, house_number, street_name, suburb_name, post_code)) FROM property_information WHERE tsv @@ plainto_tsquery('simple', ?1) LIMIT 1", nativeQuery = true)
         List<String> findByAddress(String address);
 
 }
