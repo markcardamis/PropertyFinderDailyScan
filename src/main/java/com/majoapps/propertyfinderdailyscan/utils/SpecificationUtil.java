5fc9ad02b808299722b1891a31442861ee8d603b
markcardamis <markncardamis@gmail.com>
2021-06-07 01:52:48 +1000

    removed querystring from println

--- src/main/java/com/majoapps/propertyfinderdailyscan/utils/SpecificationUtil.java
+++ src/main/java/com/majoapps/propertyfinderdailyscan/utils/SpecificationUtil.java
@@ -72,7 +72,6 @@ public class SpecificationUtil {
                 sb.append(notifications.getStreetFrontageMax());
             }
         }
-        System.out.println(sb.toString());
         return sb.toString();
     }
 
