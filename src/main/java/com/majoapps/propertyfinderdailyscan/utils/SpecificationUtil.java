f4ed45e12f762cd27c795c896517e77d4d987f74
markcardamis <markncardamis@gmail.com>
2021-06-07 00:23:32 +1000

    added specification string util

--- src/main/java/com/majoapps/propertyfinderdailyscan/utils/SpecificationUtil.java
+++ src/main/java/com/majoapps/propertyfinderdailyscan/utils/SpecificationUtil.java
@@ -1,8 +1,80 @@
 package com.majoapps.propertyfinderdailyscan.utils;
 
 import com.majoapps.propertyfinderdailyscan.business.domain.PropertyListingDTO;
+import com.majoapps.propertyfinderdailyscan.data.entity.Notifications;
 
 public class SpecificationUtil {
+
+    public static String createSpecificationString(Notifications notifications) {
+        StringBuilder sb = new StringBuilder("id>0");
+        if (notifications != null) {
+            if (notifications.getPropertyId() != null && notifications.getPropertyId() != 0) {
+                sb.append(" AND propertyId:");
+                sb.append(notifications.getPropertyId());
+            }
+            if (notifications.getPropertyZone() != null) {
+                sb.append(" AND zone:");
+                sb.append(notifications.getPropertyZone());
+            }
+            if (notifications.getPropertyAreaMin() != null && notifications.getPropertyAreaMin() != 0) {
+                sb.append(" AND area>");
+                sb.append(notifications.getPropertyAreaMin());
+            }
+            if (notifications.getPropertyAreaMax() != null && notifications.getPropertyAreaMax() != 0) {
+                sb.append(" AND area<");
+                sb.append(notifications.getPropertyAreaMax());
+            }
+            if (notifications.getPropertyPriceMin() != null && notifications.getPropertyPriceMin() != 0) {
+                sb.append(" AND priceInt>");
+                sb.append(notifications.getPropertyPriceMin());
+            }
+            if (notifications.getPropertyPriceMax() != null && notifications.getPropertyPriceMax() != 0) {
+                sb.append(" AND priceInt<");
+                sb.append(notifications.getPropertyPriceMax());
+            }
+            if (notifications.getPropertyPricePSMMin() != null && notifications.getPropertyPricePSMMin() != 0) {
+                sb.append(" AND pricePSM>");
+                sb.append(notifications.getPropertyPricePSMMin());
+            }
+            if (notifications.getPropertyPricePSMMax() != null && notifications.getPropertyPricePSMMax() != 0) {
+                sb.append(" AND pricePSM<");
+                sb.append(notifications.getPropertyPricePSMMax());
+            }
+            if (notifications.getPropertyPostCode() != null) {
+                sb.append(" AND postCode:");
+                sb.append(notifications.getPropertyPostCode());
+            }
+            if (notifications.getPropertyPriceToLandValueMin() != null) {
+                sb.append(" AND priceToLandValue>");
+                sb.append(notifications.getPropertyPriceToLandValueMin());
+            }
+            if (notifications.getPropertyPriceToLandValueMax() != null) {
+                sb.append(" AND priceToLandValue<");
+                sb.append(notifications.getPropertyPriceToLandValueMax());
+            }
+            if (notifications.getPropertyFloorSpaceRatioMin() != null) {
+                sb.append(" AND floorSpaceRatio>");
+                sb.append(notifications.getPropertyFloorSpaceRatioMin());
+            }
+            if (notifications.getPropertyFloorSpaceRatioMax() != null) {
+                sb.append(" AND floorSpaceRatio<");
+                sb.append(notifications.getPropertyFloorSpaceRatioMax());
+            }
+            if (notifications.getLandOnly() != null && notifications.getLandOnly()) {
+                sb.append(" AND propertyType:VacantLand");
+            }
+            if (notifications.getStreetFrontageMin() != null) {
+                sb.append(" AND streetFrontage>");
+                sb.append(notifications.getStreetFrontageMin());
+            }
+            if (notifications.getStreetFrontageMax() != null) {
+                sb.append(" AND streetFrontage<");
+                sb.append(notifications.getStreetFrontageMax());
+            }
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
 
     public static String createSpecificationString (PropertyListingDTO propertyInformation) {
         StringBuilder sb = new StringBuilder("propertyId>0");
